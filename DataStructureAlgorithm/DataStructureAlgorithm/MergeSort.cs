2e8ce8a6429fe0819ca101f84e2dcc973d9a2620
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-23 20:18:03 +0530

    [Merge Sort an array string,]

---
+++ DataStructureAlgorithm/DataStructureAlgorithm/MergeSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    internal class MergeSort
+    {
+        public static void Sort(List<string> arr)
+        {
+            if (arr == null || arr.Count <= 1)
+                return;
+
+            MergeSortHelper(arr, 0, arr.Count - 1);
+        }
+
+        private static void MergeSortHelper(List<string> arr, int lo, int hi)
+        {
+            if (lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+
+                MergeSortHelper(arr, lo, mid);
+                MergeSortHelper(arr, mid + 1, hi);
+
+                Merge(arr, lo, mid, hi);
+            }
+        }
+
+        private static void Merge(List<string> arr, int lo, int mid, int hi)
+        {
+            int leftSize = mid - lo + 1;
+            int rightSize = hi - mid;
+
+            List<string> leftArr = new List<string>(leftSize);
+            List<string> rightArr = new List<string>(rightSize);
+
+            for (int i = 0; i < leftSize; i++)
+                leftArr.Add(arr[lo + i]);
+
+            for (int j = 0; j < rightSize; j++)
+                rightArr.Add(arr[mid + 1 + j]);
+
+            int leftIndex = 0, rightIndex = 0;
+            int mergeIndex = lo;
+
+            while (leftIndex < leftSize && rightIndex < rightSize)
+            {
+                if (String.Compare(leftArr[leftIndex], rightArr[rightIndex]) <= 0)
+                {
+                    arr[mergeIndex] = leftArr[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    arr[mergeIndex] = rightArr[rightIndex];
+                    rightIndex++;
+                }
+                mergeIndex++;
+            }
+
+            while (leftIndex < leftSize)
+            {
+                arr[mergeIndex] = leftArr[leftIndex];
+                leftIndex++;
+                mergeIndex++;
+            }
+
+            while (rightIndex < rightSize)
+            {
+                arr[mergeIndex] = rightArr[rightIndex];
+                rightIndex++;
+                mergeIndex++;
+            }
+        }
+    }
+}
--- DataStructureAlgorithm/DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/DataStructureAlgorithm/Program.cs
@@ -1,28 +1,35 @@
-﻿using System;
+﻿using DataStructureAlgorithm;
+using System;
 namespace DataStructureAloorithm
 {
     class Program
     {
         public static void Main(string[] args) 
         {
-            int[] array = { 92, 15, 22, 90, 11,34 };
-            int temp;
-            for (int j = 0; j <= array.Length - 2; j++)
+
+            Console.WriteLine("hello mergeSort");
+
+            List<string> arr = new List<string> { "zebra", "apple", "banana", "orange", "grape" };
+
+            Console.WriteLine("Before sorting:");
+            PrintList(arr);
+
+            MergeSort.Sort(arr);
+
+            Console.WriteLine("\nAfter sorting:");
+            PrintList(arr);
+        }
+
+        private static void PrintList(List<string> arr)
+        {
+            foreach (string item in arr)
             {
-                for (int i = 0; i <= array.Length - 2; i++)
-                {
-                    if (array[i] > array[i + 1])
-                    {
-                        temp = array[i + 1];
-                        array[i + 1] = array[i];
-                        array[i] = temp;
-                    }
-                }
+                Console.Write(item + " ");
             }
-            Console.WriteLine("Sorted:");
-            foreach (int p in array)
-                Console.Write(p + " ");
-            Console.Read();
+            Console.WriteLine();
+
+
+
         }
     
     }
