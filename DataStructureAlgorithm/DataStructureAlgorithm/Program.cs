5aec067bc26e1ffc0dff33f65d7f302ae844cb7f
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-23 18:13:02 +0530

    [Check if the arrays returned by two string functions are equal.]

--- DataStructureAlgorithm/DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/DataStructureAlgorithm/Program.cs
@@ -6,6 +6,36 @@ namespace DataStructureAloorithm
         public static void Main(string[] args) 
         {
             Console.WriteLine("Hello Data Structure Algorithm Program");
+
+            {
+                string str = "SUDHIR";
+                char[] MyCharArray = str.ToCharArray();
+                Permute(MyCharArray, 0, 2);
+            }
+
+            static void Permute(char[] arry, int i, int n)
+            {
+                int j;
+                if (i == n)
+                    Console.WriteLine(arry);
+                else
+                {
+                    for (j = i; j <= n; j++)
+                    {
+                        Swap(ref arry[i], ref arry[j]);
+                        Permute(arry, i + 1, n);
+                        Swap(ref arry[i], ref arry[j]); //backtrack
+                    }
+                }
+            }
+
+            static void Swap(ref char a, ref char b)
+            {
+                char tmp;
+                tmp = a;
+                a = b;
+                b = tmp;
+            }
         }
     }
 }
